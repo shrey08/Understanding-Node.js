a83febe52dd233d88f68a9882a7982e2b2f3c690
shrey08 <shreyarora1967@gmail.com>
2020-07-30 20:01:03 +0530

    demo chat app generation

--- project/server.js
+++ project/server.js
@@ -4,6 +4,15 @@ var app=express()
 
 app.use(express.static(__dirname))
 
+var messages =[
+    {name:"Tim", message:"Hi"},
+    {name:"James",message:"Hello"}
+]
+
+app.get('/messages', (req,res) =>{
+    res.send(messages)
+})
+
 var server=app.listen(3000, () => {
     console.log('server is running on port', server.address().port)
 })
