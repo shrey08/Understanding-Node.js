1abab994a1c38fba785708a4eca38ce9af0581d4
shrey08 <shreyarora1967@gmail.com>
2020-08-01 16:02:04 +0530

    Connect to socket.io and created socket.io event

--- project/server.js
+++ project/server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyparser= require('body-parser')
 var app=express()
+var http=require('http').Server(app)
+var io=require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyparser.json())
@@ -14,9 +16,14 @@ app.get('/messages', (req,res) =>{
 
 app.post('/messages', (req,res) =>{
     messages.push(req.body)
+    io.emit('message',req.body)
     res.sendStatus(200)
 })
 
-var server=app.listen(3000, () => {
+io.on('connection', (socket)=>{
+    console.log('a user connected')
+})
+
+var server=http.listen(3000, () => {
     console.log('server is running on port', server.address().port)
 })
