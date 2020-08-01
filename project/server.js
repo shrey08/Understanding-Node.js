64bcc2f9b30bbadc747c89ea7f8b1b35bca9230a
shrey08 <shreyarora1967@gmail.com>
2020-08-01 13:34:12 +0530

    post message sevice

--- project/server.js
+++ project/server.js
@@ -4,18 +4,16 @@ var app=express()
 
 app.use(express.static(__dirname))
 app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: false}))
 
-var messages =[
-    {name:"Tim", message:"Hi"},
-    {name:"James",message:"Hello"}
-]
+var messages =[]
 
 app.get('/messages', (req,res) =>{
     res.send(messages)
 })
 
 app.post('/messages', (req,res) =>{
-    console.log(req.body)
+    messages.push(req.body)
     res.sendStatus(200)
 })
 
